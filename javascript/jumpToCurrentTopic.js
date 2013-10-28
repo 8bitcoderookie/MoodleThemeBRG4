5e0bbd00ba0a40c15716e4ad3ba2012d70d60aa8
Michael A. Rundel <coderookie@gmx.at>
2013-10-28 05:25:17 +0100

    bugfix: JS error when no current topic is set

--- javascript/jumpToCurrentTopic.js
+++ javascript/jumpToCurrentTopic.js
@@ -3,12 +3,15 @@
 // Author: Michael Rundel 
 // Description: When displaying course topics jump to current topic
 // Date: 19.08.2013
-// 22.08.2013 - YUI3 implementation
+// - 28.10.2013: Bugfix: no JS error when there is no current topic
 ///////////////////////////////////////////////
 
 YUI().use('node', function (Y) {
 	if (window.location.hash == '') {
-		window.location.hash = '#'+Y.one('.current').get('id');
+		var firstCurrentTopics = Y.one('.current');
+		if (firstCurrentTopics) { // might be null
+			window.location.hash = '#'+firstCurrentTopics.get('id');
+		}
 	}
 });
 
