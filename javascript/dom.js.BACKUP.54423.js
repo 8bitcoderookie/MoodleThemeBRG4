49c5a72e2f8635b6037caf95bfb3484b0a739312
administrator <administrator@brg4.ac.at>
2015-05-22 08:20:44 +0200

    CSS add extra space before label headings

---
+++ javascript/dom.js.BACKUP.54423.js
@@ -0,0 +1,48 @@
+<<<<<<< HEAD
+
+///////////////////////////////////////////////
+// Name: dom.js
+// Author: Michael Rundel 
+// Description: DOM manipulation helper functions
+// 22.08.2011
+///////////////////////////////////////////////
+
+brg4.setFormElementValueById = function(id, val){
+	document.getElementById(id).value = val;
+}
+
+brg4.selectOptionByValue = function(selObjName, val){
+	var selObj = document.getElementsByName(selObjName)[0];
+	var opt = selObj.options;
+	for(var i = 0; i < opt.length; i++) {
+		if (opt[i].value == val) {
+			selObj.selectedIndex = i;
+			break;
+		}
+	}
+}
+=======
+
+///////////////////////////////////////////////
+// Name: dom.js
+// Author: Michael Rundel 
+// Description: DOM manipulation helper functions
+// 22.08.2011
+///////////////////////////////////////////////
+
+brg4.setFormElementValueById = function(id, val){
+	document.getElementById(id).value = val;
+}
+
+// usage: selectOptionByValue(nameOfFormElement, valueOfSelectOption)
+brg4.selectOptionByValue = function(selObjName, val){
+	var selObj = document.getElementsByName(selObjName)[0];
+	var opt = selObj.options;
+	for(var i = 0; i < opt.length; i++) {
+		if (opt[i].value == val) {
+			selObj.selectedIndex = i;
+			break;
+		}
+	}
+}
+>>>>>>> c3ce5f352453e1dc20fa6d19db8c929cdae74261
